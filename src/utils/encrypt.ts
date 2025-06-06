74496234e79b76d7d6efcd8da4f553ebb687a15c
MisterChatlinks <166874035+MisterChatlinks@users.noreply.github.com>
2025-06-06 01:48:01 +0000

    modifyed import stmt to requiere only needed fn

--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -1,8 +1,8 @@
-import * as openpgp from 'openpgp';
+import { encrypt, createMessage } from "openpgp"; 
 
 export async function encryptPayload(payload: string, apiKey: string): Promise<string> {
-  return await openpgp.encrypt({
-    message: await openpgp.createMessage({ text: payload }),
+  return await encrypt({
+    message: await createMessage({ text: payload }),
     passwords: [apiKey],
     format: 'armored',
   });
